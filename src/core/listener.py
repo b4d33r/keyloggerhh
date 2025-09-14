fb845e3f452d90c739001fda56934eb09ca178c0
b4d33r <jakoutbadr20@gmail.com>
2025-09-14 17:07:13 +0100

    listener mzyan db wa9ila

--- src/core/listener.py
+++ src/core/listener.py
@@ -1,5 +1,17 @@
 from pynput import keyboard
 class Keystrokelistener:
     def __init__(self,on_press_callback):
-    self.on_press_callback=on_press_callback
-    self.listener=pynput.keyboard.Listener
+        self.on_press_callback=on_press_callback
+        self.listener=keyboard.Listener(on_press=self._on_press)
+    def _on_press(self,key):
+        try:
+            key=key.char
+        except AttributeError:
+            key=str(key)
+        self.on_press_callback(key)
+    def start(self):
+        self.listener.start()
+    def join(self):
+        self.listener.join()
+    def stop(self):
+        self.listener.stop()
